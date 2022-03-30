d39a4382415c67ca906845e0a66894b450aeb5b9
linghao <linghao@qq.com>
2022-03-30 23:42:55 +0800

    hot-fix commit2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,5 +6,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
