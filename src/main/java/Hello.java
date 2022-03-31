b3810d48738ae9706662d04955a922c9cf75f60a
linghao <linghao@qq.com>
2022-03-31 19:45:45 +0800

    push commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("push test!");
     }
 }
